0a6800abe801d5d646181b69b20711a8a6178268
ASBHYCEPE <ashby.cepe@unc.edu.ph>
2022-05-15 22:25:55 +0800

    Added model for the dropdown menu

---
+++ lib/models/academic_program.dart
@@ -0,0 +1,53 @@
+class AcademicProgram {
+  static final List<String> department = [
+    'Arts and Science',
+    'Business and Accountancy',
+    'Computer Studies',
+    'Criminal Justice Education',
+    'Education',
+    'Engineering and Architecture',
+    'Nursing'
+  ];
+
+  static final List<String> ASCourse = [
+    'Bachelor of Science in Biology',
+    'Bachelor of Arts in Psychology',
+    'Bachelor of Arts in Political Science',
+  ];
+
+  static final List<String> BACourse = [
+    'Bachelor of Science in Accountancy',
+    'Bachelor of Science in Busniess and Administration',
+    'Bachelor of Science in Entrepreneurship',
+  ];
+
+  static final List<String> CSCourse = [
+    'Bachelor of Science in Information Technology',
+    'Bachelor of Science in Computer Science',
+    'Bachelor of Library and Information Science',
+  ];
+
+  static final List<String> CJECourse = [
+    'Bachelor of Science in Criminal Justice Education',
+  ];
+
+  static final List<String> educCourse = [
+    'Bachelor of Elementary Education',
+    'Bachelor of Secondary Education',
+    'Bachelor of Physical Education',
+  ];
+
+  static final List<String> EACourse = [
+    'Bachelor of Science in Architecture',
+    'Bachelor of Science in Civil Engineering',
+    'Bachelor of Science in Computer Engineering',
+    'Bachelor of Science in Electrical Engineering',
+    'Bachelor of Science in Electronics Engineering',
+    'Bachelor of Science in Mechanical Engineering',
+  ];
+
+  static final List<String> nursCourse = [
+    'Caregiving NCII',
+    'Bachelor of Science in Nursing',
+  ];
+}
