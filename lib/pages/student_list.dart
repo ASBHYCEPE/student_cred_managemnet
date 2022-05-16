7049a3b895d63807810edfd7dad8482319b34226
ASBHYCEPE <ashby.cepe@unc.edu.ph>
2022-05-16 12:39:53 +0800

    Added code for generarting a list of student from result query

--- lib/pages/student_list.dart
+++ lib/pages/student_list.dart
@@ -1,4 +1,7 @@
+import 'package:cred_management/models/student.dart';
 import 'package:cred_management/pages/student_details.dart';
+import 'package:cred_management/services/database_helper.dart';
+import 'package:cred_management/widget/student_card.dart';
 import 'package:flutter/material.dart';
 
 class StudentList extends StatefulWidget {
@@ -34,7 +37,74 @@ class _StudentListState extends State<StudentList> {
         },
         child: const Icon(Icons.person_add),
       ),
-      body: Container(),
+      body: Padding(
+        padding: const EdgeInsets.fromLTRB(20, 20, 20, 0),
+        child: FutureBuilder<List<Student>?>(
+            future: DatabaseHelper.instance.getAllStudent(),
+            builder:
+                (BuildContext context, AsyncSnapshot<List<Student>?> snapshot) {
+              if (snapshot.connectionState == ConnectionState.waiting) {
+                return const CircularProgressIndicator();
+              } else if (snapshot.hasError) {
+                return Center(child: Text(snapshot.error.toString()));
+              } else if (snapshot.hasData) {
+                if (snapshot.data != null) {
+                  return ListView.builder(
+                      itemCount: snapshot.data!.length,
+                      itemBuilder: (context, index) => StudentCard(
+                            student: snapshot.data![index],
+                            onTap: () async {
+                              await Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                      builder: (context) => StudentDetails(
+                                            student: snapshot.data![index],
+                                            appBarTitle: 'UPDATE STUDENT',
+                                          )));
+                              setState(() {});
+                            },
+                            onLongPress: () async {
+                              showDialog(
+                                  context: context,
+                                  builder: (context) {
+                                    return AlertDialog(
+                                      title: const Text(
+                                          'Are your sure you want to delete this?'),
+                                      actions: [
+                                        ElevatedButton(
+                                          child: const Text('YES'),
+                                          style: ButtonStyle(
+                                              backgroundColor:
+                                                  MaterialStateProperty.all(
+                                                      Colors.red)),
+                                          onPressed: () async {
+                                            await DatabaseHelper.instance
+                                                .deleteStudent(
+                                                    snapshot.data![index]);
+                                            Navigator.pop(context);
+                                            setState(() {});
+                                          },
+                                        ),
+                                        ElevatedButton(
+                                            child: const Text('NO'),
+                                            style: ButtonStyle(
+                                                backgroundColor:
+                                                    MaterialStateProperty.all(
+                                                        Colors.green)),
+                                            onPressed: () =>
+                                                Navigator.pop(context))
+                                      ],
+                                    );
+                                  });
+                            },
+                          ));
+                }
+              }
+              return const Center(
+                child: Text('NO AVAILABLE STUDENTS'),
+              );
+            }),
+      ),
     );
   }
 }
