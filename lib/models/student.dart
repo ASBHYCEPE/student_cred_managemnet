761e96da6d39b9d45992a3508f6c110decdc78f2
ASBHYCEPE <ashby.cepe@unc.edu.ph>
2022-05-16 10:20:18 +0800

    Added fields and method for student model

---
+++ lib/models/student.dart
@@ -0,0 +1,41 @@
+class Student {
+  final String studentID;
+  final String firstName;
+  final String lastName;
+  final String middleName;
+  final String admissionYear;
+  final String course;
+  final String department;
+  final DateTime filingDate;
+
+  Student(
+      {required this.studentID,
+      required this.firstName,
+      required this.lastName,
+      required this.middleName,
+      required this.admissionYear,
+      required this.department,
+      required this.course,
+      required this.filingDate});
+
+  factory Student.fromJson(Map<String, dynamic> json) => Student(
+      studentID: json['studentID'],
+      firstName: json['firstName'],
+      lastName: json['lastName'],
+      middleName: json['middleName'],
+      admissionYear: json['admissionYear'],
+      course: json['course'],
+      department: json['department'],
+      filingDate: DateTime.parse(json['filingDate']));
+
+  Map<String, dynamic> toJson() => {
+        'studentID': studentID,
+        'firstName': firstName,
+        'lastName': lastName,
+        'middleName': middleName,
+        'admissionYear': admissionYear,
+        'course': course,
+        'department': department,
+        'filingDate': filingDate.toIso8601String()
+      };
+}
